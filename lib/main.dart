d288ad2e81e7960f58bf0324aec19dc4091b5468
almejandrx <alehrafar@gmail.com>
2024-09-09 17:50:08 -0400

    layout builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -73,46 +74,50 @@ class _MyHomePageState extends State<MyHomePage> {
     default:
       throw UnimplementedError('no widget for $selectedIndex');
 }
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Inicio'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favoritos'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.settings),
+                      label: Text('Configuración')
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.info),
+                      label: Text('Acerca de'))
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+        
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
                 ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
                 ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.settings),
-                  label: Text('Configuración')
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.info),
-                  label: Text('Acerca de'))
-              ],
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (value) {
-
-                setState(() {
-                  selectedIndex = value;
-                });
-              },
-            ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: page,
-            ),
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
