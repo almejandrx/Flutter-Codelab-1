03b6eaa7f9238e1780a0a9ece288960823e93fa8
almejandrx <alehrafar@gmail.com>
2024-09-13 14:55:44 -0400

    Esta es mi prueba

--- lib/main.dart
+++ lib/main.dart
@@ -335,7 +335,7 @@ class GeneratorPage extends StatelessWidget {
                 onPressed: () {
                   appState.getNext();
                 },
-                child: Text('Next'),
+                child: Text('Next siguiente carlos'),
               ),
             ],
           ),
